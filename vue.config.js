ac15e23c3c56d431191f893ccbf2834210b9aae6
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-05-16 22:00:45 +0900

    test build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/memorizing_quiz_app/'
+    : '/',
   outputDir: 'docs/',
   transpileDependencies: [
     'vuetify'
